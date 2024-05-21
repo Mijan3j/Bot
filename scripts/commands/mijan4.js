db4ea96f640594ef90f2681169f45f5f82fa3c2f
Mijan3j <133229714+Mijan3j@users.noreply.github.com>
2024-05-21 16:41:48 +0600

    Create mijan4.js

---
+++ scripts/commands/mijan4.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "iloveu",
+  version: "2.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "",
+  prefix: false,
+  category: "user",
+  usages: "",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("sinha")==0 || (event.body.indexOf("Sinha")==0 || (event.body.indexOf("SINHA")==0 || (event.body.indexOf("SinHa")==0)))) {
+		var msg = {
+				body: " বস 𝗠𝗜𝗭𝗔𝗡 এর বউকে তুই কেন ডাকিস ☹️😾:))"
+    }
+			api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+								       }
+
+  
