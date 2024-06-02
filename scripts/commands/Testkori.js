7ed5c0693a9a8593039839a506214da11f8feac4
Mijan3j <133229714+Mijan3j@users.noreply.github.com>
2024-06-02 10:43:38 +0600

    Update Testkori.js

--- scripts/commands/Testkori.js
+++ scripts/commands/Testkori.js
@@ -1,12 +1,13 @@
-const fs = global.nodemodule["fs-extra"];
+const fs = require("fs");
 module.exports.config = {
   name: "goibot",
-  version: "1.0.1",
-  hasPermssion: 0,
-  credits: "noyon",
-  description: "goibot",
-  commandCategory: "Noprefix",
-  usages: "noprefix",
+  version: "2.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "",
+  prefix: false,
+  category: "user",
+  usages: "goibot",
   cooldowns: 5,
 };
 module.exports.handleEvent = async function({ api, event, args, Threads, Users }) {
