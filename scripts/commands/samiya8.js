40740b9e27f3cacd830ce26c6029bc8a5b1d030d
Mijan3j <133229714+Mijan3j@users.noreply.github.com>
2024-06-20 13:10:08 +0600

    Create samiya8.js

---
+++ scripts/commands/samiya8.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "love,",
+  version: "2.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "",
+  prefix: false,
+  category: "user",
+  usages: "",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("heda")==0 || (event.body.indexOf("sawya")==0 || (event.body.indexOf("nunu")==0 || (event.body.indexOf("sawya 👅")==0)))) {
+		var msg = {
+				body: "ছি ছি ki bole asob 🐸😂🤞:))"
+    }
+			api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
